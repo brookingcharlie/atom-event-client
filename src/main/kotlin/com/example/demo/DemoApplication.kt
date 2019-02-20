a59ad8320a7740b71a3802e60a55e2f25a0b0ad5
Charlie Brooking <cbrookin@thoughtworks.com>
2019-02-20 18:13:06 +1000

    initial consumer of multipart/alterntive

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,13 +1,40 @@
 package com.example.demo
 
+import com.rometools.rome.feed.atom.Feed
+import com.rometools.rome.io.WireFeedInput
+import com.rometools.rome.io.XmlReader
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.ByteArrayOutputStream
+import java.net.URL
+import java.util.Base64
+import javax.mail.internet.MimeMultipart
+import javax.mail.util.ByteArrayDataSource
+
 
 @SpringBootApplication
 class DemoApplication : CommandLineRunner {
     override fun run(vararg args: String) {
-        System.out.println("Running!")
+        val url = "http://localhost:8080/feed"
+        val feed = WireFeedInput().build(XmlReader(URL(url))) as Feed
+        println("Feed: ${feed.title}")
+
+        for (entry in feed.entries) {
+            println("Entry: ${entry.title}")
+
+            val content = entry.contents[0]
+            val base64Encoded: String = content.value
+            val base64Decoded: ByteArray = Base64.getDecoder().decode(base64Encoded)
+            val multipart = MimeMultipart(ByteArrayDataSource(base64Decoded, content.type))
+
+            for (i in 0 until multipart.count) {
+                val part = multipart.getBodyPart(i)
+                val partBytes = ByteArrayOutputStream().also { part.inputStream.copyTo(it) }.toByteArray()
+                val partString = String(partBytes, Charsets.UTF_8)
+                println("Part ${i}: ${part.contentType}: ${partString}")
+            }
+        }
     }
 }
 
